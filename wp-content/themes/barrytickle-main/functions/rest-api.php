28d11b84c3a4f2348bcbd4e7fcae19d987ff58ff
Barry Tickle <barry.tickle12@gmail.com>
2025-02-06 18:30:32 +0000

    Added parse for repeater fields into the api

--- wp-content/themes/barrytickle-main/functions/rest-api.php
+++ wp-content/themes/barrytickle-main/functions/rest-api.php
@@ -73,6 +73,46 @@ function get_rest_api_url() {
     return esc_url_raw(rest_url('custom/v1'));
 }
 
+function sort_repeater_fields($fields) {
+    // Get the repeater field, this bit is getting the label of the repeater field, so we can find the other repeater fields, e.g. buttons_0_button_text
+    $repeater_field = array_filter($fields, function($field) {
+        return $field['field_type'] === 'repeater';
+    });
+    $repeater_field = reset($repeater_field); // This will get the first repeater field in the array
+    $label = $repeater_field['field_name']; // This will be the label of the repeater field
+
+
+    $parsed_repeater = array(); // This will store the parsed repeater fields
+    $new_fields = array(); // Will contain the new $fields array without the repeater fields
+
+    foreach($fields as $field){
+        // If the field is not part of the repeater, add it to the new fields array
+        if (strpos($field['field_name'], $label . '_') !== 0) {
+            array_push($new_fields, $field);
+            continue;
+        };
+
+        // If the field is part of the repeater, parse the field name and add it to the parsed repeater array
+        $parts = explode('_', $field['field_name']);
+
+        if(!isset($parsed_repeater[$parts[1]])) $parsed_repeater[$parts[1]] = array();
+
+        array_push($parsed_repeater[$parts[1]], array(
+            'field_name' => $parts[2],
+            'field_value' => $field['field_value']
+        ));
+
+    }
+
+    // New fields will be a new array with only the parsed_repeater field, removing the original repeater fields.
+    array_push($new_fields, array(
+        'field_name' => 'parsed_repeater',
+        'field_value' => $parsed_repeater));
+
+    
+    return $new_fields;
+}
+
 // When you use a page link in wordpress, it will return the full url of the page. This function will remove the base url from the link and return the relative path.
 function parse_link($value){
     $url = esc_url_raw(rest_url('custom/v1'));
@@ -80,6 +120,7 @@ function parse_link($value){
 
     if (strpos($value['url'], $cleaned_url) !== false) {
         $value['url'] = '/'.str_replace($cleaned_url, '', $value['url']);
+
     }
     
     $link = array(
@@ -131,6 +172,15 @@ function get_content_by_post_id($post_id) {
                     ]);
                 }
             }
+
+
+            //Checks if the block has a repeater field
+            $has_repeater = count(array_filter($fields, function($field) {
+                return $field['field_type'] === 'repeater';
+            })) > 0;
+
+            if($has_repeater) $fields = sort_repeater_fields($fields);
+
 
             array_push($acf_data, [
                 'block_name' => $block['blockName'],
